839d1f6775dad578e04dcf014d10126e8d9f00e8
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2024-12-05 19:26:20 +0400

    Update run.py

--- needlehaystack/run.py
+++ needlehaystack/run.py
@@ -31,6 +31,7 @@ class CommandArgs():
     document_depth_percent_interval_type: Optional[str] = "linear"
     num_concurrent_requests: Optional[int] = 1
     save_results: Optional[bool] = True
+    trust_remote_code: Optional[bool] = True
     results_dir: Optional[str] = "results"
     save_contexts: Optional[bool] = True
     final_context_length_buffer: Optional[int] = 200
@@ -66,7 +67,7 @@ def get_model_to_test(args: CommandArgs) -> ModelProvider:
         case "anthropic":
             return Anthropic(model_name=args.model_name)
         case "huggingface":
-            return HuggingFace(model_name=args.model_name)
+            return HuggingFace(model_name=args.model_name, trust_remote_code=args.trust_remote_code)
         case _:
             raise ValueError(f"Invalid provider: {args.provider}")
 
